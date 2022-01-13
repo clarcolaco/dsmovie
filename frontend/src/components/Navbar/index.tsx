658b7679320025e376df712c06ecfb8fc0bcbbde
Clarissa Colaco <clarissa.colaco@hotmail.com>
2022-01-12 23:30:26 -0300

    Adding navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,23 @@
+
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+import './styles.css';
+
+export default function Navbar() {
+      return (
+            //html semantico
+
+            <header>
+                  <nav className="container">
+                        <div className="dsmovie-nav-content">
+                              <h1>DSMovie</h1>
+                              <a href="https://github.com/clarcolaco/">
+                                    <div className="dsmovie-contact-container">      <GithubIcon />
+                                          <p className="dsmovie-contact-link">/clarcolaco</p>
+                                    </div>
+                              </a>
+                        </div>
+                  </nav>
+            </header>
+      );
+
+}
